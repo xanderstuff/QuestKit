3374eddc2fe3c1bbb2370e60f317a036daf1de7c
xanderstuff <68503800+xanderstuff@users.noreply.github.com>
2023-06-05 12:53:45 -0500

    add `/reloadScripts` command

--- src/main/kotlin/io/github/xanderstuff/questkit/QuestKitPlugin.kt
+++ src/main/kotlin/io/github/xanderstuff/questkit/QuestKitPlugin.kt
@@ -1,5 +1,12 @@
 package io.github.xanderstuff.questkit
 
+import dev.jorel.commandapi.CommandAPI
+import dev.jorel.commandapi.CommandAPIBukkitConfig
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.executors.CommandExecutor
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.Style
 import org.bukkit.plugin.java.JavaPlugin
 
 class QuestKitPlugin : JavaPlugin() {
@@ -10,15 +17,45 @@ class QuestKitPlugin : JavaPlugin() {
 
     override fun onEnable() {
         instance = this
+
         // ensure config file exists
         saveDefaultConfig()
 
+        // setup commands
+        CommandAPI.onLoad(CommandAPIBukkitConfig(this).verboseOutput(true))
+        registerCommands()
+
+        // load scripts
+        reloadAllScripts()
+
         @Suppress("DEPRECATION")
         log("${this.name} version ${description.version} enabled!")
-        reloadAllScripts()
     }
 
     override fun onDisable() {
         log("${this.name} disabled.")
+    }
+
+    private fun registerCommands() {
+        CommandAPICommand("reloadScripts").executes(
+            CommandExecutor { sender, _ ->
+                val (numScriptsLoaded, numScriptsAvailable) = reloadAllScripts()
+                if (numScriptsLoaded == numScriptsAvailable) {
+                    sender.sendMessage(
+                        Component.text(
+                            "$numScriptsLoaded scripts successfully loaded!",
+                            Style.style(NamedTextColor.GREEN)
+                        )
+                    )
+                } else {
+                    sender.sendMessage(
+                        Component.text(
+                            "$numScriptsLoaded scripts loaded, ${numScriptsAvailable - numScriptsLoaded} scripts failed to load. Check the log for details.",
+                            Style.style(NamedTextColor.RED)
+                        )
+                    )
+                }
+            }
+        ).register()
     }
 }
